a1f1134a4807140cfa99ab82b05d993727d1f1b0
Zichen Luo <iluo@163.com>
2019-04-29 16:08:26 +0800

    Commit codes

---
+++ codes/BrewDay/src/RecipeIngredient.java
@@ -0,0 +1,12 @@
+
+public class RecipeIngredient extends Ingredient {
+
+	public RecipeIngredient() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void updateAmount(double amount) {
+		this.amount = amount;
+	}
+
+}
