1d7a838c4dc9414cf9cbd6c26590fbd1cbb8ee78
Zichen Luo <iluo@163.com>
2019-05-18 14:20:56 +0800

    Update StorageIngredient.java

--- codes/BrewDay/src/StorageIngredient.java
+++ codes/BrewDay/src/StorageIngredient.java
@@ -1,7 +1,6 @@
 public class StorageIngredient extends Ingredient {
     StorageIngredient(String name, double amount) {
         super(name, amount);
-        // TODO Auto-generated constructor stub
     }
 
     public void AddAmount(double amount) {
