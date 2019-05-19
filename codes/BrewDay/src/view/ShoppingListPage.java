59f709b668c484d3a6952294b70f0ea998d1b9ef
Zichen Luo <iluo@163.com>
2019-05-20 03:41:36 +0800

    Create ShoppingListPage.java

---
+++ codes/BrewDay/src/view/ShoppingListPage.java
@@ -0,0 +1,88 @@
+package view;
+
+import controller.Controller;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ShoppingListPage extends JFrame {
+    private Controller controller;
+
+    public ShoppingListPage() {
+        JFrame frame = new JFrame("Brew Day!");
+
+        Container container = getContentPane();
+
+        /* ---------- Title Panel ---------- */
+        JPanel titlePanel = new JPanel(new BorderLayout());
+        titlePanel.setPreferredSize(new Dimension(600, 100));
+        JLabel pageTitle = new JLabel("Notes:", SwingConstants.CENTER);
+        titlePanel.add(pageTitle);
+
+
+        /* ---------- blank Panel ---------- */
+        JPanel blankPanelL = new JPanel(new BorderLayout());
+        blankPanelL.setPreferredSize(new Dimension(100, 100));
+        JPanel blankPanelR = new JPanel(new BorderLayout());
+        blankPanelR.setPreferredSize(new Dimension(100, 100));
+
+        /* ---------- motion Panel ---------- */
+        JPanel motionPanel = new JPanel(new GridLayout(3, 5));
+        motionPanel.setPreferredSize(new Dimension(100, 100));
+
+        JButton backBtn = new JButton("Back");
+        backBtn.setPreferredSize(new Dimension(100, 50));
+        backBtn.addActionListener(e -> {
+            controller = Controller.GetInstance();
+            controller.getMainPage(frame);
+        });
+
+        motionPanel.add(new JLabel());
+        motionPanel.add(new JLabel());
+        motionPanel.add(new JLabel());
+        motionPanel.add(new JLabel());
+        motionPanel.add(new JLabel());
+        motionPanel.add(new JLabel());
+        motionPanel.add(new JLabel());
+        motionPanel.add(backBtn);
+        motionPanel.add(new JLabel());
+        motionPanel.add(new JLabel());
+        motionPanel.add(new JLabel());
+        motionPanel.add(new JLabel());
+        motionPanel.add(new JLabel());
+        motionPanel.add(new JLabel());
+
+        /* ---------- Content Panel ---------- */
+        JPanel notePanel = new JPanel(new BorderLayout());
+        notePanel.setPreferredSize(new Dimension(200, 600));
+
+        JList<String> jList = new JList<>();
+
+        DefaultListModel<String> listModel = new DefaultListModel<>();
+
+        listModel.addElement("dddd");
+
+        jList.setModel(listModel);
+
+        jList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        JScrollPane listScrollPane = new JScrollPane(jList);
+        notePanel.add(listScrollPane, BorderLayout.CENTER);
+
+        container.add(notePanel, BorderLayout.CENTER);
+        container.add(titlePanel, BorderLayout.NORTH);
+        container.add(blankPanelR, BorderLayout.EAST);
+        container.add(blankPanelL, BorderLayout.WEST);
+        container.add(motionPanel, BorderLayout.SOUTH);
+
+        frame.add(container);
+        frame.setResizable(false);
+        frame.setSize(new Dimension(600, 800));
+        frame.setLocation(150, 150);
+        //frame.setAlwaysOnTop(true);
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        frame.setVisible(true);
+    }
+}
