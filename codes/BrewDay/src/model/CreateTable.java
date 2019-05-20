d773e7ef5f448f0cfd29f100c1859ce00a2649b3
Zichen Luo <iluo@163.com>
2019-05-20 14:52:36 +0800

    Rename CT

--- codes/BrewDay/src/model/CreateTable.java
+++ codes/BrewDay/src/model/CreateTable.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
-public class Cerf_BrewDay_CreateTable {
+public class CreateTable {
     public static void main(String[] args) {
         Connection c;
         Statement stmt;
